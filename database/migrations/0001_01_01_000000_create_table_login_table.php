bd3146f229f0a3a47b2d874f26cfba8a885d3e45
esavelliant14 <esa.freelance@gmail.com>
2025-02-09 16:05:57 +0700

    ganti tabel

--- database/migrations/0001_01_01_000000_create_table_login_table.php
+++ database/migrations/0001_01_01_000000_create_table_login_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('table_login', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('company');
+            $table->string('privileged');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -42,7 +44,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('table_login');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
