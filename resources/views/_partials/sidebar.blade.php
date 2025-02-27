4dfec089c6b952117b7d033a2e72cec82182293e
esavelliant14 <esa.freelance@gmail.com>
2025-02-27 20:35:00 +0700

    edit fitur sidebar

--- resources/views/_partials/sidebar.blade.php
+++ resources/views/_partials/sidebar.blade.php
@@ -23,7 +23,7 @@
                                     <span key="t-dashboards">NAS</span>
                                 </a>
                                 <ul class="sub-menu" aria-expanded="false">
-                                    <li><a href="{{ url('nas/nas') }}" key="t-tui-calendar">NAS Routers</a></li>
+                                    <li><a href="{{ url('nas/list-nas') }}" key="t-tui-calendar">NAS Routers</a></li>
                                     <li><a href="{{ url('nas/attribute') }}" key="t-full-calendar">Attributes</a></li>
                                     <li><a href="{{ url('nas/user') }}" key="t-full-calendar">User</a></li>
                                     <li>
