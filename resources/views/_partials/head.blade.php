5ae7a0895d8623b74cd9b01888f3be8e79069d3b
esavelliant14 <esa.freelance@gmail.com>
2025-09-26 02:09:28 +0700

    urutan css

--- resources/views/_partials/head.blade.php
+++ resources/views/_partials/head.blade.php
@@ -4,30 +4,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-
-    <!-- favicon -->
+    <!-- App favicon -->
     <link rel="shortcut icon" href="{{ url('/public/img/favicon.ico') }}">
 
-    <!-- Vendor CSS dulu -->
+        {{-- VITE --}}
+    
     <link href="{{ url('/public/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+
+ 
+
     <link href="{{ url('/public/libs/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App js -->
+    <script>
+        window.baseUrl = "{{ asset('public/') }}";
+
+    </script>
+    
+
+    <!-- Sweet Alert-->
     <link href="{{ url('/public/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- DataTables -->
     <link href="{{ url('/public/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ url('/public/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ url('/public/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="{{ url('/public/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />     
+    <!-- spectrum colorpicker2 -->
     <link href="{{ url('/public/libs/spectrum-colorpicker2/spectrum.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ url('/public/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="{{ url('/public/libs/@chenfengyuan/datepicker/datepicker.min.css') }}">
-
-    <!-- CSS template Skote -->
+    <!-- Bootstrap Css -->
     <link href="{{ url('/public/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
     <link href="{{ url('/public/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    
+    <!-- App Css-->
     <link href="{{ url('/public/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-    {{-- Vite CSS terakhir supaya override template jika perlu --}}
-    @vite(['resources/css/app.css'])
-
-    <script>
-        window.baseUrl = "{{ asset('public/') }}";
-    </script>
+    <script src="{{ url('/public/js/plugin.js') }}"></script>
+  @vite(['resources/css/app.css'])
 </head>
--- resources/views/_partials/js.blade.php
+++ resources/views/_partials/js.blade.php
@@ -1,50 +1,73 @@
-    <!-- Core JS -->
-    <script src="{{ url('/public/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ url('/public/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ url('/public/libs/metismenu/metisMenu.min.js') }}"></script>
-    <script src="{{ url('/public/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ url('/public/libs/node-waves/waves.min.js') }}"></script>
+<!-- JAVASCRIPT -->
+<script src="{{ url('/public/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('/public/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('/public/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ url('/public/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ url('/public/libs/node-waves/waves.min.js') }}"></script>
 
-    <!-- Charts -->
-    <script src="{{ url('/public/libs/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ url('/public/js/pages/apex.js') }}"></script>
-    <script src="{{ url('/public/js/pages/dashboard.init.js') }}"></script>
+<!-- apexcharts -->
+<script src="{{ url('/public/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ url('/public/js/pages/apex.js') }}"></script>
 
-    <!-- Datatables -->
-    <script src="{{ url('/public/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ url('/public/libs/jszip/jszip.min.js') }}"></script>
-    <script src="{{ url('/public/libs/pdfmake/build/pdfmake.min.js') }}"></script>
-    <script src="{{ url('/public/libs/pdfmake/build/vfs_fonts.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ url('/public/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
-    <script src="{{ url('/public/js/pages/datatables.init.js') }}"></script>
+<!-- dashboard init -->
+<script src="{{ url('/public/js/pages/dashboard.init.js')  }}"></script>
 
-    <!-- Sweetalert -->
-    <script src="{{ url('/public/libs/sweetalert2/sweetalert2.min.js') }}"></script>
-    <script src="{{ url('/public/js/pages/sweet-alerts.init.js') }}"></script>
+<!-- App js -->
+<script src="{{ url('/public/js/app.js') }}"></script>
 
-    <!-- Form Plugins -->
-    <script src="{{ url('/public/libs/select2/js/select2.min.js') }}"></script>
-    <script src="{{ url('/public/libs/spectrum-colorpicker2/spectrum.min.js') }}"></script>
-    <script src="{{ url('/public/libs/bootstrap-timepicker/js/bootstrap-timepicker.min.js') }}"></script>
-    <script src="{{ url('/public/libs/@chenfengyuan/datepicker/datepicker.min.js') }}"></script>
-    <script src="{{ url('/public/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
-    <script src="{{ url('/public/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}"></script>
-    <script src="{{ url('/public/libs/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
-    <script src="{{ url('/public/js/pages/form-advanced.init.js') }}"></script>
+<!-- Required datatable js -->
+<script src="{{ url('/public/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ url('/public/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<!-- Buttons examples -->
+<script src="{{ url('/public/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ url('public/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ url('/public/libs/jszip/jszip.min.js') }}"></script>
+<script src="{{ url('public/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+<script src="{{ url('/public/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+<script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ url('/public/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
 
-    <!-- Custom JS -->
-    <script>
-        window.APP_URL = "{{ url('/') }}"; 
-    </script>
-    <script src="{{ url('/public/js/pages/customize.js') }}?v={{ time() }}"></script>
+<!-- Responsive examples -->
+<script src="{{ url('/public/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ url('/public/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+
+<!-- Datatable init js -->
+<script src="{{ url('/public/js/pages/datatables.init.js') }}"></script>  
+
+<!-- Sweet Alerts js -->
+<script src="{{ url('/public/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+<!-- Sweet alert init js-->
+<script src="{{ url('/public/js/pages/sweet-alerts.init.js') }}"></script>
+
+<script src="{{ url('/public/libs/select2/js/select2.min.js') }}"></script>
+
+
+
+<!-- Form Advance JS -->
+
+
+<!-- Spectrum colorpicker2 -->
+<script src="{{ url('/public/libs/spectrum-colorpicker2/spectrum.min.js') }}"></script>
+<script src="{{ url('/public/libs/bootstrap-timepicker/js/bootstrap-timepicker.min.js') }}"></script>
+<script src="{{ url('/public/libs/@chenfengyuan/datepicker/datepicker.min.js') }}"></script>
+<script src="{{ url('/public/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ url('/public/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}"></script>
+<script src="{{ url('/public/libs/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
+<script src="{{ url('/public/libs/select2/js/select2.min.js') }}"></script>
+<script src="{{ url('/public/js/pages/form-advanced.init.js') }}"></script>
+
+
+<!-- CUSTOM JS -->
+<script>
+    window.APP_URL = "{{ url('/') }}"; 
+
+</script>
+<script src="{{ url('/public/js/pages/customize.js') }}?v={{ time() }}"></script>
+
+    {{-- JS vendor/library dulu --}}
     <script src="{{ url('/public/js/plugin.js') }}"></script>
-
-    {{-- Vite bundle terakhir --}}
+    {{-- Vite JS terakhir --}}
     @vite(['resources/js/app.js'])
+  
+  
